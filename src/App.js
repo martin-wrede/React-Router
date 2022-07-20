b04f300b2f533206c4adae453522f59f1988fbdb
martin-wrede <81897938+martin-wrede@users.noreply.github.com>
2022-07-20 20:11:31 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import "./styles.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Index } from "./pages";
 import { About } from "./pages/about";
-
+import { UserContext } from "./UserContext";
+// import { login } from "./utils/login";
 function AppRouter() {
+  const [user, setUser] = useState(null);
+
+  // usememo for more complex apps
+  // to create waht the value is
+  // it prevents providerValue from chaning
+  // unless value,setvalue changes
+  const value = useMemo(
+    () => ({
+      // object
+      user,
+      setUser
+    }),
+    [user, setUser]
+  );
   return (
     <Router>
       <div>
@@ -18,10 +33,12 @@ function AppRouter() {
             </li>
           </ul>
         </nav>
-        <Routes>
-          <Route path="/" exact element={<Index />} />
-          <Route path="/about/" element={<About />} />
-        </Routes>
+        <UserContext.Provider value={value}>
+          <Routes>
+            <Route path="/" exact element={<Index />} />
+            <Route path="/about/" element={<About />} />
+          </Routes>
+        </UserContext.Provider>
       </div>
     </Router>
   );
